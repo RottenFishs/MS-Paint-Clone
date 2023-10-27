9a29bfb010d6b7273f31d3d36b2d0472eb79348e
ksocher <socherk@mail.gvsu.edu>
2023-10-27 01:09:36 -0400

    Reformatted to Pep8 using Black

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
 import pygame
 import random
 import GUI
 
 # Initialize the Pygame window
-screen = pygame.display.set_mode( (512, 512), pygame.RESIZABLE )
-pygame.display.set_caption('MS Paint Clone')
+screen = pygame.display.set_mode((512, 512), pygame.RESIZABLE)
+pygame.display.set_caption("MS Paint Clone")
 run_program = True
 dim0_size = 512
 dim1_size = 512
+
 
 class Canvas:
     """A class representing a drawing canvas
@@ -22,7 +22,8 @@ class Canvas:
         undrawn (bool): A flag indicating if the canvas needs to be redrawn.
         scale (float): The scaling factor for the canvas.
     """
-    def __init__(self, width = 512, height = 512) -> None:
+
+    def __init__(self, width=512, height=512) -> None:
         """
         Initialize a new Canvas.
 
@@ -30,34 +31,31 @@ class Canvas:
             width (int): The width of the canvas.
             height (int): The height of the canvas.
         """
-        
+
         self.width = width
         self.height = height
-        
+
         self.surface = pygame.Surface((width, height))
-        self.surface.fill((255,255,255)) # test function
-        
+        self.surface.fill((255, 255, 255))  # test function
+
         self.x_offset = 0
         self.y_offset = 0
-        
+
         # if this is true, then the canvas and thus the display needs to be redrawn
         self.undrawn = False
-        
+
         self.scale = 1
 
     def _draw_(self, window_screen):
         # Internal method for drawing the canvas
-        surf_display = pygame.transform.scale(self.surface, (self.width*self.scale, self.height*self.scale))
+        surf_display = pygame.transform.scale(
+            self.surface, (self.width * self.scale, self.height * self.scale)
+        )
         pygame.Surface.blit(window_screen, surf_display, (self.x_offset, self.y_offset))
 
     def _tick_(self):
         # Internal method for updating the canvas
         self.scale *= 0.95
-        
-
-
-
-
 
 
 myCanvas = Canvas()
@@ -70,12 +68,12 @@ b_range = random.randrange(255)
 
 fill_color = (255, 255, 255)
 
-pygame.draw.rect(screen, fill_color,pygame.Rect(0,0,512,512))
+pygame.draw.rect(screen, fill_color, pygame.Rect(0, 0, 512, 512))
 
 pygame.display.flip()
 
 
-class Pencil():
+class Pencil:
     """A class representing a pencil tool for drawing on the canvas.
 
     Attributes:
@@ -83,6 +81,7 @@ class Pencil():
         drawing (bool): Flag indicating if the tool is drawing pixels on the canvas.
         previous_pos (tuple): The previous mouse cursor position.
     """
+
     def __init__(self) -> None:
         # This tool is currently selected
         self.active = True
@@ -93,13 +92,12 @@ class Pencil():
         # Some previous coords so the mouse actually draws lines
         self.previous_pos = (0, 0)
 
-
     def _mouse_down_(self):
         # Internal method for handling mouse down
         self.previous_pos = pygame.mouse.get_pos()
         self.drawing = True
 
-    def _mouse_up_(self): 
+    def _mouse_up_(self):
         # Internal method for handling mouse up
         self.previous_pos = pygame.mouse.get_pos()
         self.drawing = False
@@ -109,23 +107,16 @@ class Pencil():
         if self.drawing:
             current_pos = pygame.mouse.get_pos()
             color = main_gui.__get_selected_color__()
-            pygame.draw.line(canvas_obj.surface,color, current_pos, self.previous_pos)
+            pygame.draw.line(canvas_obj.surface, color, current_pos, self.previous_pos)
             self.previous_pos = current_pos
             pygame.display.flip()
-
-
-
-
 
 
 pencil_tool = Pencil()
 
 
 while run_program:
-
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             pygame.quit()
             run_program = False
@@ -137,7 +128,6 @@ while run_program:
             pencil_tool._mouse_up_()
 
         elif event.type == pygame.KEYDOWN:
-            
             if event.key == pygame.K_s:
                 pygame.image.save(screen, "test_file.png")
             if event.key == pygame.K_o:
@@ -152,13 +142,13 @@ while run_program:
 
     if not run_program:
         break
-    
+
     pencil_tool._tick_(myCanvas)
 
     myCanvas._draw_(screen)
 
     main_gui.__draw__()
 
-    pass 
+    pass
 
     pygame.display.flip()
