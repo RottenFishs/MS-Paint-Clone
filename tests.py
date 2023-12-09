370c1e129c7cfbfab6c0d32e6d57fc50df9dc117
Andrew Hua <andrewhua08@gmail.com>
2023-12-08 19:59:31 -0500

    Updated tests

--- tests.py
+++ tests.py
@@ -3,23 +3,18 @@ import main
 
 class TestMainMethods(unittest.TestCase):
 
-    def check_canvas_size(self):
+    def test_check_canvas_size(self):
         # Test method to check if canvas is 512x512
         self.assertEqual(main.Canvas().height, 512)
         self.assertEqual(main.Canvas().width, 512)
 
-    def check_default_tool(self):
+    def test_check_default_tool(self):
         # Test method to check if default tool is pencil
         self.assertEqual(main.main_gui.get_selected_tool(), "pencil")
 
-    def check_default_color(self):
+    def test_check_default_color(self):
         # Test method to check if the default color is black
-        self.assertEqual(main.main_gui.get_selected_colorm,(0,0,0))
-
-    def check_color_change(self):
-        # Test method to check if a change in the color is detected
-        main.main_gui.change_selected_color(12,12,12)
-        self.assertEqual(main.main_gui.get_selected_color,(12,12,12))
+        self.assertEqual(main.main_gui.get_selected_color(),(0,0,0))
 
 
 if __name__ == '__main__':
